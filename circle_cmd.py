295a138aea716c69689a2fd4f88d22321f925814
Alexey Maslennikov <leha.maslennikov2003@gmail.com>
2024-02-21 18:45:27 +0500

    Initial commit

---
+++ circle_cmd.py
@@ -0,0 +1,71 @@
+import requests
+import db
+from secret import login, password
+
+json_data = [
+    {
+        'tid': 13,
+        'data': [
+            {
+                'page': 0,
+                'start': 0,
+                'limit': 25,
+                'sort': [
+                    {
+                        'property': None,
+                        'direction': 'ASC',
+                    },
+                ],
+                'filter': [],
+            },
+        ],
+        'action': 'wow_Services',
+        'type': 'rpc',
+        'method': 'cmdGetChallenge',
+    },
+]
+
+def get_page():
+    while True:
+        response = ses.post('https://cpsl.wowcircle.me/main.php?1&serverId=null', json=json_data)
+        if response.status_code != 200:
+            print('get_page_1: ', response.status_code)
+            print('get_page_1: ',response.text)
+            exit(0)
+        if json_data[0]['data'][0]['page'] % 100 == 0:
+            print(json_data[0]['data'][0]['page'])
+        json_data[0]['data'][0]['page'] += 1
+        json_data[0]['data'][0]['start'] += 25
+        try:
+            yield response.json()
+        except Exception as e:
+            print('get_page_2: ', e)
+            print('get_page_2: ', response.status_code)
+            print('get_page_2: ', response.text)
+
+
+def parse():
+    for js in get_page():
+        try:
+            for key in js['result']['data']:
+                db.add_key(key)
+        except Exception as e:
+            print("parse_exception: ", e)
+            print(js)
+            exit(0)
+
+
+def main():
+    ses = requests.session()
+    ses.get(url='https://cpsl.wowcircle.me')
+    ses.post(url='https://cpsl.wowcircle.me/main.php?1&serverId=null', data='[{"tid":4,"data":[{"accountName":"'+login+'","password":"'+password+'","captcha":""}],"action":"wow_Services","type":"rpc","method":"cmdLogin"}]')
+    db.create()
+    parse()
+
+
+if __name__ == '__main__':
+    ses = requests.session()
+    ses.get(url='https://cpsl.wowcircle.me')
+    ses.post(url='https://cpsl.wowcircle.me/main.php?1&serverId=null', data='[{"tid":4,"data":[{"accountName":"'+login+'","password":"'+password+'","captcha":""}],"action":"wow_Services","type":"rpc","method":"cmdLogin"}]')
+    db.create()
+    parse()
---
+++ db.py
@@ -0,0 +1,98 @@
+import sqlite3 as sq
+import datetime
+
+#print(datetime.datetime.utcfromtimestamp(1705693235))
+#print(datetime.timedelta(seconds=2147))
+
+
+dung = {
+    '2284' :	'Sanguine Depths',
+    '2285' :	'Spires of Ascension',
+    '2286' :	'The Necrotic Wake',
+    '2287' : 	'Halls of Atonement',
+    '2289' :	'Plaguefall',
+    '2290' :	'Mists of Tirna Scithe',
+    '2291' :	'De Other Side',
+    '2293' :	'Theater of Pain',
+    '2441' :	'Tazavesh the Veiled Market'
+}
+
+def create():
+    '''
+    DROP IF EXIST AND CREATE mythic, characters, mythic_guid
+    Affixes ChallengeLevel Date RecordTime TimerLevel
+    guid name specID CovenantID SoulbindID'''
+    with sq.connect('data.db') as conn:
+        cur = conn.cursor()
+        cur.execute('DROP TABLE IF EXISTS mythic')
+        cur.execute('DROP TABLE IF EXISTS characters')
+        cur.execute('DROP TABLE IF EXISTS mythic_guid')
+        cur.execute(
+            '''
+            CREATE TABLE mythic (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                inst TEXT,
+                Affixes TEXT,
+                ChallengeLevel INTEGER,
+                Date TEXT,
+                RecordTime TEXT,
+                TimerLevel INTEGER
+            )'''
+        )
+        cur.execute(
+            '''
+            CREATE TABLE mythic_guid (
+                id INTEGER,  
+                guid INTEGER,
+                name TEXT,
+                specID INTEGER,
+                Ilvl INTEGER,
+                CovenantID INTEGER, 
+                SoulbindID INTEGER
+            )'''
+        )
+        cur.execute(
+            '''
+            CREATE TABLE characters (
+                guid INTEGER PRIMARY KEY,  
+                name TEXT,
+                keys TEXT
+            )'''
+        )
+
+def add_key(key: dict):
+    '''key json added in db'''
+    with sq.connect('data.db') as conn:
+        cur = conn.cursor()
+        id = cur.execute(
+            f'''
+            INSERT INTO mythic(inst, Affixes, ChallengeLevel, Date, RecordTime, TimerLevel) VALUES
+            ('{dung[key['MapID']]}', '{key['Affixes']}', {key['ChallengeLevel']}, '{datetime.datetime.utcfromtimestamp(int(key['Date']))}', '{datetime.timedelta(seconds=int(key['RecordTime']))}', {key['TimerLevel']})
+            '''
+        ).lastrowid
+        for member in key['members']:
+            cmd = f'''
+                INSERT INTO mythic_guid(id, guid, name, specID, Ilvl, CovenantID, SoulbindID) VALUES
+                ({id}, {member['guid']}, '{member['name']}', {member['specID']}, {member['Ilvl']}, {member['CovenantID']}, {member['SoulbindID']})
+                    '''
+            cur.execute(cmd)
+            keys = cur.execute(f"SELECT keys FROM characters WHERE guid={member['guid']}").fetchone()
+            if keys:
+                cur.execute(
+                    f'''UPDATE characters SET keys = '{keys[0]+' '+str(id)}' WHERE guid={member['guid']}'''
+                )
+            else:
+                cur.execute(
+                    f'''INSERT INTO characters(guid, name, keys) VALUES ({member['guid']}, '{member['name']}', '{id}')'''
+                )
+
+
+
+def get_character(name):
+      '''SELECT character whose nickname start with name'''
+      with sq.connect('data.db') as conn:
+        cur = conn.cursor()
+        res = cur.execute(
+            f'''SELECT guid, name FROM characters WHERE name LIKE '{name}%' '''
+        )
+        return res.fetchall() 
