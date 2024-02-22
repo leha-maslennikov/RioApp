69e5d0e77f5a124ff9ba375308f00c1fcb1823d7
Alexey Maslennikov <leha.maslennikov2003@gmail.com>
2024-02-22 12:27:35 +0500

    MythicData: Key, Character, KeyCharacter, Request

--- db.py
+++ db.py
@@ -4,15 +4,32 @@ from typing import Type
 
 #print(datetime.datetime.utcfromtimestamp(1705693235))
 #print(datetime.timedelta(seconds=2147))
-
+p = '''Key.ID
+Key.INST
+Key.AFFIXES
+Key.CHALLENGE_LEVEL
+Key.DATE
+Key.RECORD_TIME
+Key.TIMER_LEVEL'''.lower().split()
+for i in range(len(p)):
+    print(p[i], f'= i[{i}]')
 class KeyCharacter:
     id: int  
     guid: int
     name: str
-    spec_ID: int
+    spec_id: int
     ilvl: int
-    covenant_ID: int 
-    soulbind_ID: int
+    spec_id: int 
+    covenant_id: int
+    soulbind_id: int
+    ID = 'id'
+    GUID = 'guid'
+    NAME = 'name'
+    SPEC_ID = 'specID'
+    ILVL = 'Ilvl'
+    COVENANT_ID = 'CovenantID'
+    SOULBIND_ID = 'SoulbindID'
+
 class Key:
     id:int 
     inst: str
@@ -22,10 +39,21 @@ class Key:
     record_time: str
     timer_level: int
     characters: list[Type[KeyCharacter]]
+    ID = 'id'
+    INST = 'inst'
+    AFFIXES = 'Affixes'
+    CHALLENGE_LEVEL = 'ChallengeLevel'
+    DATE = 'Date'
+    RECORD_TIME = 'RecordTime'
+    TIMER_LEVEL = 'TimerLevel'
+
 class Character:
     guid: int
     name: str
     keys: list[Type[Key]]
+    GUID = 'guid' 
+    NAME = 'name'
+    KEYS = 'keys'
 
 
 dung = {
@@ -40,109 +68,128 @@ dung = {
     '2441' :	'Tazavesh the Veiled Market'
 }
 
-def create():
-    '''
-    DROP IF EXIST AND CREATE mythic, characters, mythic_guid
-    Affixes ChallengeLevel Date RecordTime TimerLevel
-    guid name specID CovenantID SoulbindID'''
-    with sq.connect('data.db') as conn:
-        cur = conn.cursor()
-        cur.execute('DROP TABLE IF EXISTS mythic')
-        cur.execute('DROP TABLE IF EXISTS characters')
-        cur.execute('DROP TABLE IF EXISTS mythic_guid')
-        cur.execute(
-            '''
+class Where:
+    pass
+class Request:
+    table: str
+    where: 
+
+class MythicData:
+
+    def __init__(self) -> None:
+        self.conn = sq.connect('data.db')
+        self.cur = self.conn.cursor()
+
+    def __del__(self) -> None:
+        self.cur.close()
+        self.conn.close()
+
+    def create(self):
+        '''
+        DROP IF EXIST AND CREATE mythic, characters, mythic_guid
+        Affixes ChallengeLevel Date RecordTime TimerLevel
+        guid name specID CovenantID SoulbindID'''
+        self.cur.execute('DROP TABLE IF EXISTS mythic')
+        self.cur.execute('DROP TABLE IF EXISTS characters')
+        self.cur.execute('DROP TABLE IF EXISTS mythic_guid')
+        self.cur.execute(
+            f'''
             CREATE TABLE mythic (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                inst TEXT,
-                Affixes TEXT,
-                ChallengeLevel INTEGER,
-                Date TEXT,
-                RecordTime TEXT,
-                TimerLevel INTEGER
+                {Key.ID} INTEGER PRIMARY KEY AUTOINCREMENT,
+                {Key.INST} TEXT,
+                {Key.AFFIXES} TEXT,
+                {Key.CHALLENGE_LEVEL} INTEGER,
+                {Key.DATE} TEXT,
+                {Key.RECORD_TIME} TEXT,
+                {Key.TIMER_LEVEL} INTEGER
             )'''
         )
-        cur.execute(
-            '''
+        self.cur.execute(
+            f'''
             CREATE TABLE mythic_guid (
-                id INTEGER,  
-                guid INTEGER,
-                name TEXT,
-                specID INTEGER,
-                Ilvl INTEGER,
-                CovenantID INTEGER, 
-                SoulbindID INTEGER
+                {KeyCharacter.ID} INTEGER,  
+                {KeyCharacter.GUID} INTEGER,
+                {KeyCharacter.NAME} TEXT,
+                {KeyCharacter.SPEC_ID} INTEGER,
+                {KeyCharacter.ILVL} INTEGER,
+                {KeyCharacter.COVENANT_ID} INTEGER, 
+                {KeyCharacter.SOULBIND_ID} INTEGER
             )'''
         )
-        cur.execute(
-            '''
+        self.cur.execute(
+            f'''
             CREATE TABLE characters (
-                guid INTEGER PRIMARY KEY,  
-                name TEXT,
-                keys TEXT
+                {Character.GUID} INTEGER PRIMARY KEY,  
+                {Character.NAME} TEXT,
+                {Character.KEYS} TEXT
             )'''
         )
 
-def add_key(key: dict):
-    '''key json added in db'''
-    with sq.connect('data.db') as conn:
-        cur = conn.cursor()
-        id = cur.execute(
+    def add_key(self, key: Type[Key]):
+        '''key json added in db
+        old:('{dung[key['MapID']]}', '{key['Affixes']}', {key['ChallengeLevel']}, '{datetime.datetime.utcfromtimestamp(int(key['Date']))}', '{datetime.timedelta(seconds=int(key['RecordTime']))}', {key['TimerLevel']})
+        ({id}, {member['guid']}, '{member['name']}', {member['specID']}, {member['Ilvl']}, {member['CovenantID']}, {member['SoulbindID']})
+        '''
+        id = self.cur.execute(
             f'''
-            INSERT INTO mythic(inst, Affixes, ChallengeLevel, Date, RecordTime, TimerLevel) VALUES
-            ('{dung[key['MapID']]}', '{key['Affixes']}', {key['ChallengeLevel']}, '{datetime.datetime.utcfromtimestamp(int(key['Date']))}', '{datetime.timedelta(seconds=int(key['RecordTime']))}', {key['TimerLevel']})
+            INSERT INTO mythic({Key.ID},{Key.INST},{Key.AFFIXES},{Key.CHALLENGE_LEVEL},{Key.DATE},{Key.RECORD_TIME},{Key.TIMER_LEVEL}) VALUES
+            ({key.id},{key.inst},{key.affixes},{key.challenge_level},{key.date},{key.record_time},{key.timer_level})
             '''
         ).lastrowid
-        for member in key['members']:
+        for member in key.characters:
             cmd = f'''
-                INSERT INTO mythic_guid(id, guid, name, specID, Ilvl, CovenantID, SoulbindID) VALUES
-                ({id}, {member['guid']}, '{member['name']}', {member['specID']}, {member['Ilvl']}, {member['CovenantID']}, {member['SoulbindID']})
+                INSERT INTO mythic_guid({KeyCharacter.ID}, {KeyCharacter.GUID}, {KeyCharacter.NAME}, {KeyCharacter.SPEC_ID}, {KeyCharacter.ILVL}, {KeyCharacter.COVENANT_ID}, {KeyCharacter.SOULBIND_ID}) VALUES
+                ({member.id}, {member.guid}, {member.name}, {member.spec_id}, {member.ilvl}, {member.covenant_id}, {member.soulbind_id})
                     '''
-            cur.execute(cmd)
-            keys = cur.execute(f"SELECT keys FROM characters WHERE guid={member['guid']}").fetchone()
+            self.cur.execute(cmd)
+            keys = self.cur.execute(f"SELECT {Character.KEYS} FROM characters WHERE {Character.GUID}={member.guid}").fetchone()
             if keys:
-                cur.execute(
-                    f'''UPDATE characters SET keys = '{keys[0]+' '+str(id)}' WHERE guid={member['guid']}'''
+                self.cur.execute(
+                    f'''UPDATE characters SET {Character.KEYS} = '{keys[0]+' '+str(id)}' WHERE {Character.guid}={member.guid}'''
                 )
             else:
-                cur.execute(
-                    f'''INSERT INTO characters(guid, name, keys) VALUES ({member['guid']}, '{member['name']}', '{id}')'''
+                self.cur.execute(
+                    f'''INSERT INTO characters({Character.GUID}, {Character.NAME}, {Character.KEYS}) VALUES ({member.guid}, '{member.name}', '{id}')'''
                 )
 
+    def get_character_by_name(self, name: str):
+        '''SELECT character whose nickname start with name'''
+        res = self.cur.execute(
+            f'''SELECT {Character.GUID}, {Character.NAME}, {Character.KEYS} FROM characters WHERE {Character.NAME} LIKE '{name}%' '''
+        )
+        res = []
+        for i in res.fetchall():
+            char = Character()
+            char.guid = i[0]
+            char.name = i[1]
+            char.keys = i[2]
+            res.append(char)
+        return res
+    
 
-
-def get_character_by_name(name):
-    '''SELECT character whose nickname start with name'''
-    with sq.connect('data.db') as conn:
-        cur = conn.cursor()
-        res = cur.execute(
-            f'''SELECT guid, name FROM characters WHERE name LIKE '{name}%' '''
+    def get_character_by_key_id(self, id):
+        '''SELECT characters from key with id'''
+        res = self.cur.execute(
+            f'''SELECT * FROM mythic_guid WHERE {KeyCharacter.ID} = {id}'''
         )
         return res.fetchall() 
       
 
-def get_keys(offset, limit = 10):
-    '''SELECT limit keys wit offset'''
-    with sq.connect('data.db') as conn:
-        cur = conn.cursor()
-        res = cur.execute(
+    def get_keys(self, offset, limit = 10):
+        '''SELECT limit keys wit offset'''
+        res = self.cur.execute(
             f'''SELECT * FROM mythic LIMIT {offset}, {limit} '''
         )
-        keys = {}
-        key_args = 'inst Affixes ChallengeLevel Date RecordTime TimerLevel'.split()
-        for key in res.fetchall():
-            tmp = {}
-            for i in range(len(key_args)):
-                tmp[i] = 
+        keys = []
+        for i in res.fetchall():
+            key = Key()
+            key.id = i[0]
+            key.inst = i[1]
+            key.affixes = i[2]
+            key.challenge_level = i[3]
+            key.date = i[4]
+            key.record_time = i[5]
+            key.timer_level = i[6]
+            keys.append(key)
+        return keys
 
-    
-    
-
-def get_character_by_key(id):
-    '''SELECT characters from key with id'''
-    with sq.connect('data.db') as conn:
-        cur = conn.cursor()
-        res = cur.execute(
-            f'''SELECT * FROM mythic_guid WHERE id = {id}'''
-        )
-        return res.fetchall() 
