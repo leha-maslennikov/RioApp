1dd69ad7bc5edc1a68bd35e1ff4324c47f822677
Alexey Maslennikov <leha.maslennikov2003@gmail.com>
2024-02-29 23:46:14 +0500

    sqliterequests: new: Update

--- sqliterequests.py
+++ sqliterequests.py
@@ -1,5 +1,3 @@
-from typing import Type
-
 class Table:
     name: str
 
@@ -13,7 +11,7 @@ class Request:
 class Insert(Request):
     cmd: str
 
-    def __init__(self, table: Type[Table], args: list[str], values: list) -> None:
+    def __init__(self, table: Table, args: list[str], values: list) -> None:
         args = ', '.join(args)
         values = ', '.join([f"'{i}'" if type(i) == str else str(i) for i in values])
         self.cmd = f'''INSERT INTO {table.name} ({args}) VALUES ({values})'''
@@ -24,7 +22,7 @@ class Insert(Request):
 class Select(Request):
     args: str
 
-    def __init__(self, table: Type[Table], args: list[str], all = False) -> None:
+    def __init__(self, table: Table, args: list[str], all = False) -> None:
         if all:
             args = '*'
         else:
@@ -50,5 +48,15 @@ class Order(Request):
         args = ', '.join([f"{column[i]} {'DESC' if reverse[i] else 'ASC'}" for i in range(len(column))])
         self.cmd = f'{request.get()} ORDER BY {args}'
 
+    def get(self) -> str:
+        return self.cmd
+    
+class Update(Request):
+
+    def __init__(self, table: Table, args: list[str], values: list) -> None:
+        super().__init__()
+        args = ', '.join([f"{args[i]} = '{values[i]}'" if type(values[i]) == str else f"{args[i]} = {values[i]}" for i in range(len(args))])
+        self.cmd = f'''UPDATE {table.name} SET {args}'''
+    
     def get(self) -> str:
         return self.cmd
